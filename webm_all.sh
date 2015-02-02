acccba52185610990873695ed8ee7838068fca91
Jim Vallandingham <vlandham@gmail.com>
2015-02-02 13:36:30 -0800

    webm

--- webm_all.sh
+++ webm_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for f in video/*.mp4
+for f in video_org/*.mp4
 do
   echo $f
   filename=$(basename "$f")
   extension="${filename##*.}"
   filename="${filename%.*}"
   echo $filename
-  ffmpeg -i "${f}" -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis "webm/${filename}.webm"
+  ffmpeg -i "${f}" -c:v libvpx -crf 4 -b:v 1M -c:a libvorbis "webm_org/${filename}.webm"
 done
 
 
